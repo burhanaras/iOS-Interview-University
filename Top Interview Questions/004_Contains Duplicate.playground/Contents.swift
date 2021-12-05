b87037878e15d292c2f4f6c881da979b733c9779
Burhan Aras <burhanaras@Burhans-iMac.local>
2021-12-05 12:15:46 +0300

    004.Contains Duplicate

---
+++ Top Interview Questions/004_Contains Duplicate.playground/Contents.swift
@@ -0,0 +1,38 @@
+/*
+ EASY: Contains Duplicate
+ LeetCode: https://leetcode.com/explore/interview/card/top-interview-questions-easy/92/array/578/
+ 
+ Given an integer array nums, return true if any value appears at least twice in the array, and return false if every element is distinct.
+
+  
+ Example 1:
+
+ Input: nums = [1,2,3,1]
+ Output: true
+ Example 2:
+
+ Input: nums = [1,2,3,4]
+ Output: false
+
+ 
+ */
+
+func containsDuplicate(nums: [Int]) -> Bool {
+    var dictionary = [Int: Int]()
+    for number in nums {
+        if let _ = dictionary[number] {
+            return true
+        }
+        dictionary[number] = 1
+    }
+    return false
+}
+
+/*
+    Time Complexity: O(N) Linear time
+    Space Complexity: O(1) Linear space
+ */
+
+containsDuplicate(nums: [1,2,3,1])
+containsDuplicate(nums: [1,2,3,4])
+containsDuplicate(nums: [])
